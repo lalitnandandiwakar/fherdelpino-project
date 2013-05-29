1e99cbfe42a8e332dbdd1c4b4a8182d928539c5d
fherdelpino <fherdelpino@gmail.com>
2013-05-29 02:41:42 +0000

    Share project "MP3metadata" into "https://fherdelpino-project.googlecode.com/svn"

---
+++ MP3metadata/src/com/mp3/metadata/Main.java
@@ -0,0 +1,15 @@
+package com.mp3.metadata;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Rename r = new Rename("/home/delpinof/Music/pruebas3");
+		r.renameSongs();
+		
+	}
+	
+}

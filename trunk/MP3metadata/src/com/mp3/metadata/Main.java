1c17e54c8fc274a744be662b7e0cc4b0083a08b2
fherdelpino@gmail.com <fherdelpino@gmail.com@c4470286-a15d-87b0-23db-33f729b6d62f>
2013-05-31 05:58:00 +0000

    change the user interaction

--- trunk/MP3metadata/src/com/mp3/metadata/Main.java
+++ trunk/MP3metadata/src/com/mp3/metadata/Main.java
@@ -3,15 +3,16 @@ package com.mp3.metadata;
 import javax.swing.JOptionPane;
 
 
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String s = JOptionPane.showInputDialog("Introduce la carpeta:"); 
-		Rename r = new Rename(s);
-		r.renameSongs();
+		
+		String s = JOptionPane.showInputDialog("Folder to work:"); 
+		new Rename(s).renameSongs();
 		
 	}
 	

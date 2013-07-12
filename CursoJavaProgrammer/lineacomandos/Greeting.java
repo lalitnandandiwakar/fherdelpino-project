0142d959e9f0e4683d61e119b4bf300513a9b2df
fherdelpino <fherdelpino@gmail.com>
2013-07-12 05:09:37 +0000

    Delete old code

--- CursoJavaProgrammer/lineacomandos/Greeting.java
+++
@@ -1,7 +0,0 @@
-
-public class Greeting{
-
-	public void sayHi(){
-		System.out.println("hi");
-	}
-}
--- CursoJavaProgrammer/src/com/curso/genericos/MyCollection.java
+++
@@ -1,9 +0,0 @@
-package com.curso.genericos;
-
-import java.util.List;
-
-public interface MyCollection<E, P> extends List<E> {
-
-	void setMyCollection(int index, P val);
-
-}

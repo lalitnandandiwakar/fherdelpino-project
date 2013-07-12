68afbf852d9b9b6f2acb7395a60c295e5de97230
fherdelpino@gmail.com <fherdelpino@gmail.com@c4470286-a15d-87b0-23db-33f729b6d62f>
2013-07-12 05:09:37 +0000

    Delete old code

--- trunk/CursoJavaProgrammer/lineacomandos/MiInterface.java
+++
@@ -1,6 +0,0 @@
-
-public interface MiInterface {
-	public void m1();
-	public void m2();
-	public void m3();
-}
--- trunk/CursoJavaProgrammer/src/com/curso/instanceOf/Child.java
+++
@@ -1,5 +0,0 @@
-package com.curso.instanceOf;
-
-class Child extends Parent implements MyInterface {
-
-}

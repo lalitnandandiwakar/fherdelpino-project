6f74676bcd68f34edbf893a28dd1556215388518
fherdelpino <fherdelpino@gmail.com>
2013-11-29 08:21:44 +0000

    URI and Main class fix

--- mp3player/src/com/fherdelpino/mp3player/PlayerFrame.java
+++ mp3player/src/com/fherdelpino/mp3player/PlayerFrame.java
@@ -1,4 +1,8 @@
 package com.fherdelpino.mp3player;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  *
  * @author delpinof
@@ -27,7 +31,7 @@ public class PlayerFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTextField1.setText("file:C:/Users/delpinof/Downloads/02Lateralus.mp3");
+        jTextField1.setText(EXAMPLE_TEXT);
 
         jButton1.setText("Play");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -73,11 +77,16 @@ public class PlayerFrame extends javax.swing.JFrame {
     }// </editor-fold>                        
 
     Player p = Player.getInstance();
+    private static final String EXAMPLE_TEXT = "";
     
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         // TODO add your handling code here:
+    	String path = jTextField1.getText();
+    	path = "file:///".concat(path);
+    	path = path.replace("\\", "/");
+    	path = path.replace(" ", "%20");
     	
-    	p.changeSongAndPlay(jTextField1.getText());
+    	p.changeSongAndPlay(path);
     	
     }                                        
 
@@ -86,40 +95,7 @@ public class PlayerFrame extends javax.swing.JFrame {
     	p.stop();
     }                                        
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PlayerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PlayerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PlayerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PlayerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new PlayerFrame().setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
